189df2463ff0cc26ba86969d8bb2d47e70c4cf7b
Hammad Mushtaq <hammad@4ks.io>
2022-04-09 01:36:49 -0400

    Tiltfile with go live update

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
-
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println(Hello("api"))
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Yolo What the Fuck2",
+		})
+	})
+	r.GET("/ready", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong2",
+		})
+	})
+
+	r.Run("0.0.0.0:5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
