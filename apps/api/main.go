9d21d960b7d0d3a625c14f7923a072689f94514a
Hammad Mushtaq <hammad@4ks.io>
2022-04-13 05:11:13 -0400

    Adding preliminary routes to api

--- apps/api/main.go
+++ apps/api/main.go
@@ -6,16 +6,46 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/recipes", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Yolo",
 		})
 	})
-	r.GET("/ready", func(c *gin.Context) {
+	r.POST("/recipes", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong222",
+		})
+	})
+	r.GET("/recipes/:recipeId", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "yolo",
+		})
+	})
+	r.POST("/recipes/:recipeId/star", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong2222",
+		})
+	})
+	r.POST("/recipes/:recipeId/fork", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong222",
+		})
+	})
+	r.GET("/recipes/:recipeId/revisions", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong222",
+		})
+	})
+	r.GET("/recipes/:recipeId/revisions/:revisionId", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong222",
 		})
 	})
 
+	r.GET("/ready", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong222",
+		})
+	})
 	r.Run("0.0.0.0:5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
