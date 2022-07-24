cc93e574ffb127557d4d2d0288ca0587d127c20a
Nicolas Delorme <ndelorme@costar.com>
2022-07-23 21:16:39 -0400

    purge mantine

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,47 +1,12 @@
 import React from 'react';
-import { useHotkeys, useLocalStorage } from '@mantine/hooks';
-import {
-  MantineProvider,
-  ColorSchemeProvider,
-  ColorScheme,
-} from '@mantine/core';
-
-import AppLayout from './components/layout/AppLayout';
 import { SessionContextProvider } from './providers/session-context';
 import Router from './Router';
 
-export const themeHotKey = 'mod+J';
-
 function App() {
-  const DARK = 'dark';
-  const LIGHT = 'light';
-
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
-    defaultValue: LIGHT,
-    getInitialValueInEffect: true,
-  });
-
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === DARK ? LIGHT : DARK));
-
-  useHotkeys([[themeHotKey, () => toggleColorScheme()]]);
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{ colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        <SessionContextProvider>
-          <Router />
-        </SessionContextProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <SessionContextProvider>
+      <Router />
+    </SessionContextProvider>
   );
 }
 
