5c7c34f480552e01efdae9bd2ed97aa65c357ab4
Hammad Mushtaq <hammad@4ks.io>
2022-09-27 03:01:15 -0400

    Fetch config on client side when deployed

--- apps/web/src/AppAuth.tsx
+++ apps/web/src/AppAuth.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-function AppAuth() {
+type AppConfig = {
+  AUTH0_AUDIENCE: string
+  AUTH0_DOMAIN: string
+  AUTH0_CLIENT_ID: string
+}
+
+function AppAuth({ appConfig }: {appConfig: AppConfig}) {
   return (
     <Auth0Provider
-      audience={import.meta.env.VITE_AUTH0_AUDIENCE as string}
-      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
+      audience={appConfig.AUTH0_AUDIENCE}
+      domain={appConfig.AUTH0_DOMAIN}
+      clientId={appConfig.AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
       cacheLocation={'localstorage'}
     >
