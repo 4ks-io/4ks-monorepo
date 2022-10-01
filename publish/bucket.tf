f555c2bbfdba6da576c54a2993d5260941c3ce75
pmoyo@costar.com <pmoyo@costar.com>
2022-10-01 09:54:52 -0400

    add gw

--- publish/bucket.tf
+++ publish/bucket.tf
@@ -5,3 +5,12 @@ resource "google_storage_bucket" "test_bucket" {
 
   uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket_object" "default" {
+  name         = "swagger"
+# Uncomment and add valid path to an object.
+  source       = "../apps/api/docs/swagger.json"
+  #content      = "swagger.json"
+  #content_type = "text/plain"
+  bucket       = google_storage_bucket.test_bucket.name
+}
