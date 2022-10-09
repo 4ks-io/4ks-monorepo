141d769ba7d05e3cf1c6797fa4071bce7301b223
Nicolas Delorme <nic@4ks.io>
2022-10-09 09:15:28 -0400

    cleanup entrypoint

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 import { SessionContextProvider } from './providers/session-context';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import Router from './Router';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 initializeIcons();
 
-function App() {
+type AppConfig = {
+  AUTH0_AUDIENCE: string;
+  AUTH0_DOMAIN: string;
+  AUTH0_CLIENT_ID: string;
+};
+
+function App({ appConfig }: { appConfig: AppConfig }) {
   return (
-    <SessionContextProvider>
-      <Router />
-    </SessionContextProvider>
+    <Auth0Provider
+      audience={appConfig.AUTH0_AUDIENCE}
+      domain={appConfig.AUTH0_DOMAIN}
+      clientId={appConfig.AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation={'localstorage'}
+    >
+      <SessionContextProvider>
+        <Router />
+      </SessionContextProvider>
+    </Auth0Provider>
   );
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppAuth from './AppAuth';
+import App from './App';
 
 (async () => {
   let appConfig = {
-    AUTH0_AUDIENCE: "",
-    AUTH0_DOMAIN: "",
-    AUTH0_CLIENT_ID: ""
-  }
+    AUTH0_AUDIENCE: '',
+    AUTH0_DOMAIN: '',
+    AUTH0_CLIENT_ID: '',
+  };
 
-  if(import.meta?.env?.VITE_AUTH0_AUDIENCE) {
-    appConfig.AUTH0_AUDIENCE = import.meta.env.VITE_AUTH0_AUDIENCE as string
-    appConfig.AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN as string
-    appConfig.AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID as string
+  if (import.meta?.env?.VITE_AUTH0_AUDIENCE) {
+    appConfig.AUTH0_AUDIENCE = import.meta.env.VITE_AUTH0_AUDIENCE as string;
+    appConfig.AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN as string;
+    appConfig.AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID as string;
   } else {
-    appConfig = await (await fetch("/config.json")).json()
+    appConfig = await (await fetch('/config.json')).json();
   }
 
   ReactDOM.render(
     <React.StrictMode>
-      <AppAuth appConfig={appConfig}/>
+      <App appConfig={appConfig} />
     </React.StrictMode>,
     document.getElementById('root')
   );
-  
-
 })();
-
-
-
