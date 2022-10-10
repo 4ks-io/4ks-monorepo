989093af3754f6afa3be69f7fbd0868c54a102c5
Nicolas Delorme <nic@4ks.io>
2022-10-10 14:26:08 -0400

    working dir

--- publish/net-dns.tf
+++ publish/net-dns.tf
@@ -10,8 +10,8 @@ resource "google_dns_managed_zone" "dev_4ks_io" {
 resource "google_compute_ssl_certificate" "default" {
   name_prefix = "dev-4ks-"
   description = "development 90 day cert"
-  private_key = file("./publish/tls/private.key")
-  certificate = file("./publish/tls/certificate.crt")
+  private_key = file("./tls/private.key")
+  certificate = file("./tls/certificate.crt")
 
   lifecycle {
     create_before_destroy = true
