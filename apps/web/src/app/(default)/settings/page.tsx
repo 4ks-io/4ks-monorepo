dc96010fd0ad1006e725cbb7694bd4100e476038
Nicolas Delorme <nic@4ks.io>
2023-12-03 09:39:33 +0000

    force-no-store

--- apps/web/src/app/(default)/settings/page.tsx
+++ apps/web/src/app/(default)/settings/page.tsx
@@ -8,7 +8,6 @@ import type { Metadata } from 'next';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import CircularProgress from '@mui/material/CircularProgress';
 import Stack from '@mui/material/Stack';
 import UsernameSpecification from '@/components/UsernameSpecifications';
 import DeveloperTools from '@/components/DeveloperTools';
@@ -25,23 +24,9 @@ export default async function SettingsPage({
 }: PageProps) {
   const { user } = await handleUserNavigation(Page.AUTHENTICATED);
   const session = await getSession();
-  const t = session?.accessToken;
 
-  if (!user || !user.username) {
-    return (
-      <Container maxWidth="sm" style={{ paddingTop: 40 }}>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      </Container>
-    );
+  if (!session || !session?.user || !user || !user?.username) {
+    return <div>Error</div>;
   }
 
   return (
@@ -57,18 +42,15 @@ export default async function SettingsPage({
         }}
       >
         <Container maxWidth="sm" style={{ paddingTop: 40 }}>
-          {/* <div>EVENTS: {JSON.stringify(user.events)}</div>
-        <br />
-        <div>TOKEN: Bearer {session?.accessToken}</div> */}
           <Typography variant="h4" component="h2">
             Settings
           </Typography>
           <Stack spacing={2} style={{ paddingTop: 40 }}>
             <Typography variant="subtitle1" component="h2">
-              Email: {user?.emailAddress}
+              Email: {user.emailAddress}
             </Typography>
             <Typography variant="subtitle1" component="h2">
-              Current Username: {user?.username}
+              Current Username: {user.username}
             </Typography>
 
             <UpdateUsername username={user.username} />
--- apps/web/src/app/app/auth/[auth0]/route.ts
+++ apps/web/src/app/app/auth/[auth0]/route.ts
@@ -1,10 +1,12 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+
+export const fetchCache = 'force-no-store';
 
 export const GET = handleAuth({
   login: handleLogin({
     authorizationParams: {
       audience: process.env.AUTH0_AUDIENCE,
-      scope: "openid profile email offline_access",
+      scope: 'openid profile email offline_access',
     },
   }),
 });
